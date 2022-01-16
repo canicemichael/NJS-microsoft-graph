401556789866186ce8a0b0e1da84dd083435bf40
canicemichael <canicemichael@gmail.com>
2022-01-16 08:29:53 +0100

    cr calendar.hbs

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const calendarRouter = require('./routes/calendar');
 
 var app = express();
 
@@ -81,6 +82,15 @@ app.use(function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+var hbs = require('hbs');
+var parseISO = require('date-fns/parseISO');
+var formatDate = require('date-fns/format');
+// Helper to format date/time sent by Graph
+hbs.registerHelper('eventDateTime', function(dateTime) {
+  const date = parseISO(dateTime);
+  return formatDate(date, 'M/d/yy h:mm a');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -89,6 +99,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/calendar', calendarRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
